8fecf6d7ea65a0eb78883c7dee100af50e1ae4ef
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-03-31 17:41:25 -0500

    Frustratingly had to restart yet again, but finally have a working modal form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,9 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Need to make an object for the tasks
-function Task (title, description, dueDate) {
-    this.id = generateTaskId()
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.color = 'normal'
-}
-
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    const newId = nextId;
-    nextId++;
-    localStorage.setItem('nextId', JSON.stringify(nextId));
-    console.log('This is the ID:' + newId)
-    return newId;
+
 }
 
 // Todo: create a function to create a task card
@@ -47,12 +34,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    // The below makes the add task button open the modal when clicked
-    $('#addTaskButton').click(function() {
-        $('#taskModal').modal('show');
-    })
-    // The below lets the modal close button close the modal when clicked
-    $('.btn-close').click(function() {
-        $('#taskModal').modal('hide');
-    })
+
 });
