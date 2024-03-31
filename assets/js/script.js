84b5240e547e171224a15c1dae45f64e2871201b
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-03-30 22:12:33 -0500

    starting over

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,27 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+// Need to make an object for the tasks
+function Task (title, description, dueDate) {
+    this.id = generateTaskId()
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.color = 'normal'
+}
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     const newId = nextId;
     nextId++;
     localStorage.setItem('nextId', JSON.stringify(nextId));
-    console.log(newId)
+    console.log('This is the ID:' + newId)
     return newId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const toDoCards = document.getElementById('todo-cards')
 
-    const card = document.createElement('div');
-    card.classList.add('task-card', 'mb-2', 'card');
-    card.setAttribute('draggable', true)
-    card.dataset.taskId = task.id
-
-    const cardTitle = document.createElement('h3')
-    cardTitle.textContent = task.title
-    cardTitle.classList.add('card-title')
-
-    const cardDescription = document.createElement('p')
-    cardDescription.textContent = task.description
-    cardDescription.classList.add('card-description')
-
-    const cardDueDate = document.createElement('p')
-    cardDueDate.textContent = task.dueDate
-    cardDueDate.classList.add('card-due-date')
-
-    card.appendChild(cardTitle);
-    card.appendChild(cardDescription);
-    card.appendChild(cardDueDate);
-    toDoCards.appendChild(card);
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -46,20 +33,6 @@ function renderTaskList() {
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
 
-    const title = document.getElementById('taskTitle').value;
-    const description = document.getElementById('taskDescription').value;
-    const dueDate = document.getElementById('taskDueDate').value;
-    
-    const newTask = Task(title, description, dueDate);
-    tasks.push(newTask);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    renderTaskList();
-
-    document.getElementById('taskForm').reset();
-    var taskModalEl = document.getElementById('taskModal')
-    var taskModal = bootstrap.Modal.getIntstance(taskModalEl)
-    taskModal.hide();
 }
 
 // Todo: create a function to handle deleting a task
