796f10da355f9d10d78ca972f6006da994b232c0
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-03-31 20:10:59 -0500

    finally got localstorage to work, needed '.val' instead of '.value' among other things

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,38 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
+function handleAddTask(event) {
+    // The below prevents the page from reloading when we click our button
+    event.preventDefault();
+  
+    // The below gathers the inputs from our form and stores them as variables
+    const title = $('#taskTitle').val().trim();
+    const description = $('#taskDescription').val().trim();
+    const dueDate = $('#taskDueDate').val();
+  
+    // The below creates a Javascript object with our inputs
+    const newTask = {
+      id: generateTaskId(),
+      title: title,
+      description: description,
+      dueDate: dueDate,
+      status: 'to-do'
+    };
+  
+    // The below states that if we don't have a task list already, we'll create an empty object for it
+    if (!taskList) {
+      taskList = [];
+    }
+    // The below pushes our new task object to our task list
+    taskList.push(newTask);
+    // The below stringifies the object and pushes it to our local storage
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+  
+    // The below closes the modal and clears the form
+    $('#formModal').modal('hide');
+    $('#taskForm').trigger('reset');
+  }
+  
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
@@ -34,5 +63,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    $('#taskForm').submit(handleAddTask);
 });
